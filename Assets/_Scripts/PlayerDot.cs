df82b4a6cb151a657ca43732212942c55417b19f
thino036 <thino5016@gmail.com>
2024-11-29 17:04:17 -0600

    Changes

---
+++ Assets/_Scripts/PlayerDot.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerDot : MonoBehaviour
+{
+    public RectTransform playerDot;
+    public Transform playerObject;
+    public RectTransform sliderTransform;
+    public float maxHeight = 100f;
+    public float minHeight = -30f;
+
+    void Update()
+    {
+        if (playerDot != null && playerObject != null && sliderTransform != null)
+        {
+            float normalizedY = (playerObject.position.y - minHeight) / (maxHeight - minHeight);
+
+            float sliderHeight = sliderTransform.rect.height;
+
+            // Move the PlayerDot proportionally on the slider
+            playerDot.anchoredPosition = new Vector2(
+                playerDot.anchoredPosition.x,
+                Mathf.Clamp(normalizedY * sliderHeight, 0, sliderHeight)
+            );
+        }
+    }
+}
