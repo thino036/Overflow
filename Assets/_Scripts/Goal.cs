2dc8f88c49862bad956228cbcfbe281a2f9fd3aa
JL0244 <jl0244@uah.edu>
2024-11-30 13:08:32 -0600

    TimerUI, Highscore, EndGame logic.
    Added highscore using playerprefs. When the game ends the UI is removed and the EndGameCanvas appears.

--- Assets/_Scripts/Goal.cs
+++ Assets/_Scripts/Goal.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     public GameObject EndGameCanvas;
+    public Timer timer;
+    public GameObject UICanvas;
+    
+    private float highScore;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetFloat("HighScore", Mathf.Infinity);
+
+        if (timer == null)
+        {
+            timer = FindObjectOfType<Timer>();
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +27,14 @@ public class Goal : MonoBehaviour
         {
             EndGameCanvas.SetActive(true);
             Time.timeScale = 0f;
+            UICanvas.SetActive(false);
+
+            if (timer.GetTime() < highScore)
+            {
+                highScore = timer.GetTime();
+                PlayerPrefs.SetFloat("HighScore", highScore);
+                timer.updateHighScoreText();
+            }
         }
     }
 }
